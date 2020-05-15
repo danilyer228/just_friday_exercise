92fd63bd0d446c4fcb6e2eff1a3d4c4a928b75ed
Danilyer <danilsirotalol@gmail.com>
2020-05-15 14:02:38 +0300

    adopt skeleton

---
+++ src/Test.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Otis22\PhpSkelleton;
+
+class Test
+{
+
+    public function one(): int
+    {
+        return 1;
+    }
+    public function two(): int
+    {
+        return 2;
+    }
+}
